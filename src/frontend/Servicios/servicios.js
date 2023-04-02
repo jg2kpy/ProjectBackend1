ee98ec9d1d614d1edb6af1743746391d5a90def2
unknown <jlgutierrez2000@hotmail.com>
2023-04-02 15:52:03 -0400

    front de servicios totalmente implementado

--- src/frontend/Servicios/servicios.js
+++ src/frontend/Servicios/servicios.js
@@ -1,27 +1,19 @@
 // Definir la URL del servidor como una variable global
-const API_URL = 'http://localhost:8080/prueba/servicios/carga-puntos';
+const API_URL = 'http://localhost:8080/prueba/servicios';
 
 // Obtener los elementos del DOM
 const formCargarPuntos = document.getElementById("cargar-puntos");
-const tablaBody = document.getElementById("tabla-body");
 const formUsarPuntos = document.getElementById("usar-puntos");
-const editarForm = formUsarPuntos.querySelector("form");
+const formConvertir = document.getElementById("convertir");
+const codeCargarPuntos = document.getElementById("code-cargar-puntos");
+const codeUsarPuntos = document.getElementById("code-usar-puntos");
+const codeConvertir = document.getElementById("code-convertir");
 
-// Función para obtener todos los reglas de la API
-async function obtenerReglas() {
-  try {
-    const response = await fetch(API_URL);
-    const reglas = await response.json();
-    return reglas;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
-// Función para agregar un regla a la API
-async function agregarRegla(regla) {
+
+async function cargarPuntos(regla) {
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(`${API_URL}/carga-puntos`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -35,146 +27,70 @@ async function agregarRegla(regla) {
   }
 }
 
-// Función para eliminar un regla de la API
-async function eliminarRegla(id) {
-  try {
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "DELETE"
-    });
-    //como nuestro servidor no retorna nada al actualizar, retorno true
-    //const data = await response.json();
-    //return data;
-    return true;
-  } catch (error) {
-    console.error(error);
-  }
-}
+formCargarPuntos.addEventListener("submit", async (evento) => {
+  evento.preventDefault();
 
-// Función para actualizar un regla en la API
-async function actualizarRegla(id, regla) {
+  const cargaPunto = {
+    idCliente: formCargarPuntos.idCliente.value,
+    monto: formCargarPuntos.monto.value,
+  };
+
+  const resultado = await cargarPuntos(cargaPunto);
+
+  if (resultado) {
+    formCargarPuntos.reset();
+    codeCargarPuntos.innerHTML = JSON.stringify(resultado, null, 2)
+  }
+});
+
+
+async function usarPuntos(regla) {
   try {
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "PUT",
+    const response = await fetch(`${API_URL}/uso-puntos`, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(regla)
     });
-    //como nuestro servidor no retorna nada al actualizar, retorno true
-    //const data = await response.json();
-    //return data;
-    return true;
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error(error);
   }
 }
 
-// Función para crear un elemento de fila de la tabla de reglas
-function crearFilaRegla(regla) {
-  const fila = document.createElement("tr");
-
-  const id = document.createElement("td");
-  id.textContent = regla.idReglasAsigPuntos;
-  fila.appendChild(id);
-
-  const limiteInferior = document.createElement("td");
-  limiteInferior.textContent = regla.limiteInferior;
-  fila.appendChild(limiteInferior);
-
-  const limiteSuperior = document.createElement("td");
-  limiteSuperior.textContent = regla.limiteSuperior;
-  fila.appendChild(limiteSuperior);
-
-  const montoEquivalencia = document.createElement("td");
-  montoEquivalencia.textContent = regla.montoEquivalencia;
-  fila.appendChild(montoEquivalencia);
-
-  const acciones = document.createElement("td");
-  const editarBoton = document.createElement("button");
-  editarBoton.textContent = "Editar";
-  editarBoton.addEventListener("click", () => mostrarEditarFormulario(regla));
-  acciones.appendChild(editarBoton);
-  const eliminarBoton = document.createElement("button");
-  eliminarBoton.textContent = "Eliminar";
-  eliminarBoton.addEventListener("click", () => eliminarReglaClick(regla));
-  acciones.appendChild(eliminarBoton);
-  fila.appendChild(acciones);
-
-  return fila;
-}
-
-// Función para mostrar la tabla de reglas
-async function mostrarTablaReglas() {
-  const reglas = await obtenerReglas();
-
-  tablaBody.innerHTML = "";
-
-  reglas.sort((a, b) => a.idReglasAsigPuntos - b.idReglasAsigPuntos);
-
-  reglas.forEach((regla) => {
-    const fila = crearFilaRegla(regla);
-    tablaBody.appendChild(fila);
-  });
-}
-
-// Función para mostrar el formulario de edición de regla
-function mostrarEditarFormulario(regla) {
-  editarForm["editar-id"].value = regla.idReglasAsigPuntos;
-  editarForm["editar-limiteInferior"].value = regla.limiteInferior;
-  editarForm["editar-limiteSuperior"].value = regla.limiteSuperior;
-  editarForm["editar-montoEquivalencia"].value = regla.montoEquivalencia;
-
-  // habilito el display del form
-  formUsarPuntos.style.display = "block";
-}
-
-// Función para ocultar el formulario de edición de regla
-function ocultarEditarFormulario() {
-  formUsarPuntos.style.display = "none";
-}
-
-// Función para agregar un regla al hacer clic en el botón de enviar formulario
-formCargarPuntos.addEventListener("submit", async (evento) => {
+formUsarPuntos.addEventListener("submit", async (evento) => {
   evento.preventDefault();
 
-  const regla = {
-    limiteInferior: formCargarPuntos.limiteInferior.value,
-    limiteSuperior: formCargarPuntos.limiteSuperior.value,
-    montoEquivalencia: formCargarPuntos.montoEquivalencia.value,
+  const usarPunto = {
+    idCliente: formUsarPuntos.idCliente.value,
+    idConceptoPuntos: formUsarPuntos.idConceptoPuntos.value,
   };
 
-  const resultado = await agregarRegla(regla);
+  const resultado = await usarPuntos(usarPunto);
 
   if (resultado) {
-    formulario.reset();
-    mostrarTablaReglas();
+    formUsarPuntos.reset();
+    codeUsarPuntos.innerHTML = JSON.stringify(resultado, null, 2)
   }
 });
 
-// Función para eliminar un regla al hacer clic en el botón de eliminar en la fila de la tabla
-async function eliminarReglaClick(regla) {
-  const confirmacion = confirm(`¿Desea eliminar al regla ${regla.idReglasAsigPuntos}?`);
-  if (confirmacion) {
-    const resultado = await eliminarRegla(regla.idReglasAsigPuntos);
-    mostrarTablaReglas();
-  }
-}
 
-// Función para actualizar un regla al hacer clic en el botón de enviar formulario de edición
-editarForm.addEventListener("submit", async (evento) => {
+
+formConvertir.addEventListener("submit", async (evento) => {
   evento.preventDefault();
 
-  const regla = {
-    limiteInferior: editarForm["editar-limiteInferior"].value,
-    limiteSuperior: editarForm["editar-limiteSuperior"].value,
-    montoEquivalencia: editarForm["editar-montoEquivalencia"].value
-  };
-  const resultado = await actualizarRegla(editarForm["editar-id"].value, regla);
-  if (resultado) {
-    ocultarEditarFormulario();
-    mostrarTablaReglas();
+  const monto = formConvertir.monto.value
+
+  try {
+    const response = await fetch(`${API_URL}/conv-monto/${monto}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    codeConvertir.innerHTML = JSON.stringify(data, null, 2)
+    return data;
+  } catch (error) {
+    console.error(error);
   }
 });
-
-// Mostrar la tabla de reglas cuando se cargue la página
-mostrarTablaReglas();
